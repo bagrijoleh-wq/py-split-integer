e3c7e4fdd8ca3542998cf1e2f6604cd0b75f6073
Oleg <bagrijoleh@gmail.com>
2026-01-06 15:55:43 +0200

    Solutoin

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,7 +2,7 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    assert sum(split_integer(6, 2)) == 6
+    assert sum(split_integer(26, 6)) == 26
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
@@ -10,7 +10,7 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    assert sum(split_integer(8, 1)) == 8
+    assert split_integer(8, 1) == [8]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
@@ -22,9 +22,4 @@ def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
 
 
 def test_quantity_elements_should_be_equal_to_number_of_parts() -> None:
-    assert len(split_integer(8, 2)) == 2
-
-
-def test_difference_between_min_max_should_be_less_than_one() -> None:
-    result = split_integer(21, 5)
-    assert max(result) - min(result) <= 1
+    assert len(split_integer(25, 8)) == 8
